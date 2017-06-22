8c59f8729418fff3beed44568e589d1ba23e08cc
Zihao Xu <zxql2015@mymail.pomona.edu>
2017-06-22 14:10:04 -0700

    Code for running simulations

--- Rcode/Rcode - Functions to generate all kinds of data.R
+++ Rcode/Rcode - Functions to generate all kinds of data.R
@@ -14,25 +14,59 @@ makeLinearDF <- function(numobs, b, constant){
 
 
 
-makeClusteredDF <- function(numobs, centers, sd){
-  values <- c()
-  yexp <- c()
-  m = length(centers)
-  numobs <- floor(numobs/m)
-  for(i in 1:m){
-    values <- c(values, rnorm(m*numobs, centers[i], sd))
-    yexp <- c(yexp, rep(i*runif(1,-50, 50), numobs))
+# makeClusteredDF borrowed from Benji Lu
+
+#####
+# generates data that is clustered. the response values of observations within a given cluster are drawn from the same normal distribution
+#
+# num.dim (double): number of predictor variables for the generated dataset
+# sample.size (double): sample size for the generated dataset
+# num.clusters (double): number of clusters into which the data will be divided. Each cluster will contain roughly, but not necessarily exactly, the same number of observations.
+# NOTE: num.clusters must be greater than or equal to 2
+# cluster.means (vector): a vector of length num.clusters specifying the mean response value for observations in each cluster
+# NOTE: length of vector must equal num.clusters
+# error.SD (double/vector): the standard deviation of the error term. If constant.SD is FALSE, this must be a vector of length num.clusters
+# seed (double): seed state
+# constant.SD (boolean): TRUE if the standard deviation of the error term is the same for each cluster, FALSE if the standard deviation of the error term is not the same for each cluster
+# RETURNS: a generated sample in a sample.size x (num.dim + 1) dataframe
+#
+#####
+makeClusteredDF <- function(num.dim, sample.size, num.clusters, cluster.means, error.SD, seed, constant.SD = TRUE) {
+  
+  # required package (run -install.packages("MixSim")- in the console to install if you haven't already)
+  require(MixSim)
+  
+  # set seed
+  # set.seed(seed)
+  
+  # use the MixSim package to generate a finite mixture model with Gaussian components with 0 average overlap and 0 maximum overlap of clusters desired
+  repeat {
+    mixModel <- MixSim(BarOmega = 0, MaxOmega = 0, K = num.clusters, p = num.dim)
+    if (mixModel$fail == 0) break
   }
-  x <- matrix(values, nrow = m*numobs, byrow = TRUE, ncol = m)
-  y <- yexp+runif(numobs, -1,1)
-  df <- data.frame(y, x, yexp)
-  for(col in 2:m+1){
-    print(as.integer(substr(colnames(df)[col], 2, 2)))
-    if(as.integer(substr(colnames(df)[col], 2, 2))%%2==0){
-      df[col] <- -df[col]
-    }
+  
+  # use the MixSim package to generate explanatory variable values for the observations, as well as a label for the cluster to which the observation belongs
+  data.points <- simdataset(n = sample.size, Pi = mixModel$Pi, Mu = mixModel$Mu, S = mixModel$S)
+  
+  # if error standard deviation is the same for all clusters
+  if (constant.SD) {
+    # each observation's response value is its cluster's mean response plus an error term with fixed standard deviation
+    data.points$y <- cluster.means[data.points$id] + rnorm(sample.size, 0, error.SD)
+    data.points$yexp <- cluster.means[data.points$id]
+  } else {
+    # each observation's response value is its cluster's mean response plus an error term with cluster-specific standard deviation
+    data.points$yexp <- cluster.means[data.points$id]
+    data.points$y <- cluster.means[data.points$id] + rnorm(sample.size, 0, error.SD[data.points$id])
   }
-  return(df)
+  
+  # save sample
+  #write.csv(data.points, paste0("clustered", "Mean", paste0(cluster.means, collapse = "_"), "SD", paste0(error.SD, collapse = "_"), "Dataset.csv"), row.names = FALSE)
+  
+  # convert to dataframe and remove cluster label
+  data.points <- as.data.frame(cbind(y = data.points$y, xvals = data.points$X, yexp = data.points$yexp))
+  
+  # return dataframe
+  return(data.points)
 }
 
 
--- Test/Test - Tuning for *s gamma.R
+++ Test/Test - Tuning for *s gamma.R
@@ -1,47 +1,74 @@
 library(randomForest)
 library(MASS)
-library(caret)
+library(MixSim)
 library(doParallel)
-registerDoParallel(cores = detectCores())
+registerDoParallel(cores = detectCores()-10)
+print(paste("detectCores()-10 = ", detectCores()-10))
+source('/home/zxu/func.R')
+
+#library(devtools)
+#install_github("zihaoxu/randomForestBLB")
 
 
+makeTestTrainList <- function(N, k, ndim, linear = TRUE, clustered = FALSE, cosine = FALSE, seed = 47){
+      set.seed(seed)
 
-createSplits <- function(df, nfolds = 10){
-  folds <- createFolds(df[,1], k = nfolds)
-  subsamples <- lapply(folds, function(ind, dat) dat[ind,], dat = df)
-  return(subsamples)
+      if(linear){
+          data.all <- makeLinearDF(N + N/k, seq(5,5*ndim,5), 2)
+      }else if(clustered){
+          num.cluster = 25
+          data.all <- makeClusteredDF(num.dim = ndim, sample.size = N+N/k, num.clusters = num.cluster, cluster.means = seq(10,10*num.cluster,10), error.SD = 1, seed = 47)
+      }else if(cosine){
+          data.all <- makeCosineDF(N + N/k, numberX = ndim)
+      }
+  
+      test.index <- sample(1:(N + N/k), (N/k))
+      data.test <- data.all[test.index,]
+      rownames(data.test) <- 1:nrow(data.test)
+      
+      data.train <- data.all[-test.index,]
+      rownames(data.train) <- 1:nrow(data.train)
+      return(list(data.train, data.test))
+}
+
+createSplits <- function(df, gamma, s){
+    rownames(df) <- 1:nrow(df)
+    n <- nrow(df)
+    allIndex <- 1:n 
+    subsamples <- list()
+    for(i in 1:s){
+      r.sample.index <- sample(allIndex, n^gamma, replace = FALSE)
+      subsamples[[i]] <- df[r.sample.index,]
+    }
+    return(subsamples)
 }
 
 
-calculateMSE <- function(N, k, s, linear = TRUE, clustered = FALSE, cosine = FALSE, rep.time = 10, seed = 47){
-  result <- c()
-  set.seed(seed)
-  
-  for(q in 1:rep.time){
-    if(linear){
-      myDF <- makeLinearDF(N, c(2, 4, 6), 1)
-    }else if(clustered){
-      myDF <- makeClusteredDF(N, c(5,10,15,20), 1)
-    }else if(cosine){
-      myDF <- makeCosineDF(N)
-    }
-    testIndex <- seq(0,nrow(myDF)/k,1)
-    data.train <- myDF[-testIndex,]
-    data.test = myDF[testIndex, "y"]
-    subsamples <- createSplits(data.train, s)
-    subsampleResults <- matrix(nrow = length(testIndex))
-    subsampleResults <- foreach(ww=1:length(subsamples), .combine = cbind)%dopar%{
-          rf.boston = randomForest(y~.-yexp, subsamples[[ww]], ntree = 500, sampling_factor = s)
-          yhat.rf = predict(rf.boston, newdata = myDF[testIndex,])
-    }
-    if(s!=1){
+calculateMSE <- function(test.train.list, N, gamma, s, ntree, rep.time = 5){
+    result <- c()
+    sampling_factor = round(N/(N^gamma))
+
+    data.train <- test.train.list[[1]]
+    data.train <- data.train[,1:(ncol(data.train)-1)]
+    data.test <- test.train.list[[2]]
+    data.test.y <- data.test[, "y"]
+    pred.data <- data.test[,2:(ncol(data.test)-1)]
+    
+    for(kj in 1:rep.time){
+      subsamples <- createSplits(data.train, gamma = gamma, s=s)
+      subsampleResults <- matrix(nrow = nrow(data.test))
+      subsampleResults <- foreach(ww=1:length(subsamples), .combine = cbind)%dopar%{
+        rf = randomForest(y~., subsamples[[ww]], ntree = ntree, sampling_factor = sampling_factor)
+        yhat.rf = predict(rf, newdata = pred.data)
+      }
+      if(s!=1){
         finalPred <- rowMeans(subsampleResults)
-    }else{
+      }else{
         finalPred <- subsampleResults
+      }
+      result <- c(result, mean((finalPred - data.test.y)^2))
+      #print(result)
     }
-    result <- c(result, mean((finalPred - data.test)^2))
-    #print(result)
-  }
   return(mean(result))
 }
 
@@ -49,33 +76,49 @@ calculateMSE <- function(N, k, s, linear = TRUE, clustered = FALSE, cosine = FAL
 
 # different Ns, ks (pct), and ss
 
-Ns <- c(25,50,100,200,400)
-ks <- c(2,5)
-df <- data.frame(n = numeric(0), s = numeric(0), pct = numeric(0), MSE = numeric(0))
-ss <- c(2,4,6,8,10)
+Ns <- c(1000) # , 500, 1000, 2000, 4000, 8000, 16000, 32000, 64000, 128000
+k <- 5
+df <- data.frame(n = numeric(0), gamma = numeric(0),s = numeric(0), ntree = numeric(0), time = numeric(0),  MSE = numeric(0))
+#ss <- c(1,3,6,9,12) #seq(2,22,4)
+#ntrees <- c(100) # c(50,100,200)
+gas <- c(.5,.6, .7,.8, .9, 1) #seq(0.5,.9,.1)
 
 #mean((yhat.rf - data.test)^2)
 for(N in Ns){
-    for(k in ks){
-      for(s in ss){
-        mse <- calculateMSE(N = N, k = k, s = s)
-        print(paste("N =", N, "pct =", 1-1/k, 's =',s, 'MSE =', mse))
-        df<-rbind(df, data.frame(n = N, s = s, pct = 1-1/k, MSE = mse))
+    for(ga in gas){
+      if(ga == .5){
+        ss <- seq(1,18,3)
+        ntrees <- c(100,500,1000) 
+      }else if(ga == .6){
+        ss <- seq(1,18,3)
+        ntrees <- c(100,500,1000) 
+      }else if(ga == .7){
+        ss <- seq(1,18,3)
+        ntrees <- c(50,100,500)
+      }else if(ga == .8){
+        ss <- seq(1,15,3)
+        ntrees <- c(50,100,500)
+      }else if(ga == .9){
+        ss <- seq(1,15,3)
+        ntrees <- c(25,100,250)
+      }else{
+        ss <- c(1,3,5)
+        ntrees <- c(25,100,250)
+      }
+      for(ntree in ntrees){
+        for(s in ss){
+          start.time <- Sys.time()
+          train.test.list <- makeTestTrainList(N, k, ndim = 10, linear = FALSE, clustered  = TRUE)
+          mse <- calculateMSE(train.test.list, N = N, gamma = ga, s = s, ntree = ntree)
+          time.taken <- Sys.time() - start.time
+          print(paste("N =", N, 'gamma = ', ga,'s =',s, 'ntree =',ntree, "time =", time.taken, 'MSE =', mse))
+          df<-rbind(df, data.frame(n = N, gamma = ga, s = s, ntree = ntree, time = time.taken, MSE = mse))
+        }
       }
     }
 }
-#write.csv(df, './CSV/Linear_n_s_pct_MSE.csv')
-
-
-
-
-
-
-
-library(ggplot2)
-
-ggplot(df, aes(x=gamma, y=MSE, col=as.factor(pct))) + geom_line()
-
+write.csv(df, 'Clustered_n_s_pct_MSE.csv')
+#write.csv(df, '/Users/zihaoxu/R_repos/BLB-RF-Sim/CSV/Clustered_n_s_pct_MSE.csv')
 
 
 
